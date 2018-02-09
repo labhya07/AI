75ecf14824f6f25b261153dd82cbb1d3283fd75e
labhya07 <33115078+labhya07@users.noreply.github.com>
2018-02-09 11:42:00 +0530

    Update palindrome q13.pl

--- palindrome q13.pl
+++ palindrome q13.pl
@@ -1,5 +1,5 @@
 
-************** PROGRAM *************
+
 
 conc([],L,L).
 conc([X|Y],M,[X|N]):- conc(Y,M,N).
@@ -9,11 +9,3 @@ compare([],[]).
 compare([A|B],[C|D]):- A=C,compare(B,D).
 palindrome(L):- reverse1(L,R),compare(R,L).
 
-
-************** OUTPUT *************
-
-palindrome([a,b,c,d,d,c,b,a]).
-true.
-
-palindrome([a,b,b,c]).
-false.
